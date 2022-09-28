fd362ec5a5d9807c96b54e82bdf22bf436939529
joephy527 <joephy527@gmail.com>
2022-09-28 13:24:12 -0700

    solutions to the rest of the tasks

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,15 +1,3 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
-        return None
-    else:
-        keys = sorted(a_dictionary.keys())
-        for i in keys:
-            max = a_dictionary[keys[0]]
-            if a_dictionary[i] >= max:
-                max = a_dictionary[i]
-            else:
-                return max
-    for i in a_dictionary:
-        if a_dictionary[i] == max:
-            return i
+    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
