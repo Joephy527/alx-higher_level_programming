9033820486d46f989e6d0181ac9e174ed042f7ab
joephy527 <joephy527@gmail.com>
2022-10-11 10:55:59 -0700

    update solution

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -21,6 +21,7 @@ class Square:
             raise ValueError('size must be >= 0')
         else:
             self.__size = int(size)
+
     def area(self):
         """Calculate area of the square
         Returns: The square of the size
