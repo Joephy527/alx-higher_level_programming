10a3e4d18d206edee723792ce5a9083bfeaacbba
joephy527 <joephy527@gmail.com>
2022-10-17 10:24:16 -0700

    solutions to mandatory tasks

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -62,4 +62,4 @@ class Rectangle:
 
         if self.__height == 0 or self.__width == 0:
             return 0
-        return (self.__height + self.__width) * 2
+        return ((self.__height + self.__width) * 2)
