32df44452c59320372cb3793b94472eaca4903f0
joephy527 <joephy527@gmail.com>
2022-10-31 13:30:55 -0700

    solutions to tasks

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,12 +10,17 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """initializes instances"""
 
+        self.size = size
+        self.x = x
+        self.y = y
+        self.id = None
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """Defines a format for the string representation of the class"""
 
-        return f"[square] ({self.id}) {self.x}/{self.y} - {self.size}"
+        return '[{}] ({}) {}/{} - {}'.\
+            format(type(self).__name__, self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
