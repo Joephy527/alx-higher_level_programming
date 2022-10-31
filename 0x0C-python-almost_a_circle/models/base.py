8819b45fb8db8f05d4e8487de689e1777579bf9b
joephy527 <joephy527@gmail.com>
2022-10-31 14:21:38 -0700

    solutions to tasks

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module contains a class to serve as base for other classes"""
 
+
 import csv
 import json
 import os
@@ -24,6 +25,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON representation of list_dictionaries"""
+
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         if (type(list_dictionaries) != list or not
@@ -34,6 +36,7 @@ class Base:
     @classmethod
     def save_to_file(cls, list_objs):
         """Save JSON representation to file"""
+
         file_name = cls.__name__ + ".json"
         with open(file_name, "w") as jsonfile:
             if list_objs is None:
@@ -45,6 +48,7 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """Returns list of JSON string representations"""
+
         json_string_list = []
 
         if json_string is not None and json_string != '':
@@ -57,7 +61,7 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """Returns an instance with all attributes already set"""
-        # create an instance of an existing class
+
         if cls.__name__ == 'Rectangle':
             dummy = cls(1, 1)
         elif cls.__name__ == 'Square':
