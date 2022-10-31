d20da1401cb91bb1cbf88b852ccfb3fa6617bc9c
joephy527 <joephy527@gmail.com>
2022-10-31 11:30:54 -0700

    solutions to tasks

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module contains a class to serve as base for other classes"""
+
+
+class Base:
+    """Represents base of all classes created """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """initializes the class"""
+
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
